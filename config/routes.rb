5f61d496c7eb0b259345eb5064817f92bad87951
Milan Cancarevic <milancancarevic@gmail.com>
2017-04-21 13:13:30 +0200

    toggle

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolio_items, except: [:show]
   get 'portfolio_item/:id', to: 'portfolio_items#show', as: 'portfolio_item_show'
-  resources :blogs
+
+  resources :blogs do 
+  	member do
+  		get :toggle_status
+  	end	
+  end	
 
   get 'about-me', to: 'pages#about'  # otvara na /about-me url-u
   get 'contact', to: 'pages#contact'  # otvara na /contact url-u
