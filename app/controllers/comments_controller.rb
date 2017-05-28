5b8c903c9c6daad7a0c6b7257501e6539b4f64a9
Milan Cancarevic <milancancarevic@gmail.com>
2017-05-28 13:23:10 +0200

    error no route matches2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
-    debugger
   end
 
   private
--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -26,7 +26,6 @@ class PortfolioItemsController < ApplicationController
 
 	def create
 		@portfolio_item = PortfolioItem.new(portfolio_item_params)
-		debugger
 
 		respond_to do |format|
 			if @portfolio_item.save
